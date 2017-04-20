2dea392e2fb0caf47fab810e4298375e7703d680
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-04-19 19:34:14 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,192 +1,180 @@
 #!/usr/bin/env bash
 
-onf() {
- wget https://valtman.name/files/telegram-cli-1222
-		mv telegram-cli-1222 tg
-	  	chmod +x tg
-       chmod +x anticrash.sh
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
+
+update() {
+  git pull
+  git submodule update --init --recursive
+  install_rocks
+}
+#By: @sajjad_021
+# Will install luarocks on THIS_DIR/.luarocks
+install_luarocks() {
+  git clone https://github.com/keplerproject/luarocks.git
+  cd luarocks
+  git checkout tags/v2.4.2 # Current stable
+
+  PREFIX="$THIS_DIR/.luarocks"
+
+  ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
+
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  make build && make install
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting.";exit $RET;
+  fi
+
+  cd ..
+  rm -rf luarocks
 }
 
-declare -A logo
-    seconds="0.003"
-logo[-1]=" ::::::::::  :::::::      ::     ::  ::::::::  ::     ::  ::::::  :::::::: :::::: "
-logo[0]="     +:     :+    :+:    :+:+   +::+ +:       :+:+   +:+: +:   :+ +:       +:   :+ "
-logo[1]="     :+     +:           :+ +:+:+ :+ :+       :+ +:+:+ :+ :+   +: :+       :+   +: "
-logo[2]="     ++     :#           ++  +:+  ++ +++++#   ++  +:+  ++ #+++++  +++:+#   +++++#  "
-logo[3]="     ++     +#  +#+#+    #+   +   #+ #+       #+   +   +# #+   +# #+       #+   +# "
-logo[4]="     +#     #+     +#    +#       +# +#       +#       #+ +#    # +#       +#    #+"
-logo[5]="     ##      #######     ##       ## ######## ##       ## ####### ######## ##    ##"
-    printf "\033[32;4;208m\t"
-    for i in ${!logo[@]}; do
-        for x in `seq 0 ${#logo[$i]}`; do
-            printf "${logo[$i]:$x:1}"
-            sleep $seconds
-        done
-        printf "\n\t"
-    done
-printf "\n"
+install_rocks() {
+  ./.luarocks/bin/luarocks install luasocket
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
 
-ins1() {
-cd libs
+  ./.luarocks/bin/luarocks install oauth
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install -f
-sudo dpkg -a --configure
-sudo apt-get dist-upgrade
-sudo dpkg --configure -a
-sudo sudo apt-get dist-upgrade
+  ./.luarocks/bin/luarocks install redis-lua
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
 
-sudo apt-get install -y tor
- sudo apt-get install -y openvpn
-  sudo apt-get install network-manager-openvpn
+  ./.luarocks/bin/luarocks install lua-cjson
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
 
-sudo service tor start
- sudo service openvpn start
+  ./.luarocks/bin/luarocks install fakeredis
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
 
-export http_proxy="socks5://127.0.0.1:9150"
- export https_proxy="socks5://127.0.0.1:9150"
-  export ftp_proxy="socks5://127.0.0.1:9150"
-  
-		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-	   	sudo apt-get install g++-4.7 -y c++-4.7 -y
-	    	sudo apt-get update
-          sudo apt-get upgrade
-        
-	        	sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
-            
-            		sudo apt-get install screen -y
-               		sudo apt-get install tmux -y
-                 		sudo apt-get install libstdc++6 -y
-                   		sudo apt-get install lua-lgi -y
-	                     	sudo apt-get install libnotify-dev -y
-                        
-cd ..
+  ./.luarocks/bin/luarocks install xml
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install feedparser
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install serpent
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
 }
 
-ins2() {
-sudo apt-get update
-sudo apt-get install -y lua5.3 liblua5.3-dev git redis-server libssl-dev fortune-mod fortunes cowsay fortune unzip make libexpat1-dev libcurl3 libcurl3-gnutls libcurl4-openssl-dev ruby ruby-dev
-sudo gem install redis-dump
-git clone http://github.com/keplerproject/luarocks
-cd luarocks
-./configure --lua-version=5.3 --versioned-rocks-dir --lua-suffix=5.3
-make build
-sudo make install
-sudo luarocks-5.3 install --server=http://luarocks.org/dev openssl
-rocklist="luasocket luasec multipart-post lpeg dkjson serpent redis-lua luafilesystem oauth uuid html-entities luaossl feedparser lua-curl telegram-bot-lua"
-for rock in $rocklist; do
-    sudo luarocks-5.3 install $rock
-done
-sudo -k
-cd ..
+install2() {
+  	sudo apt-get update
+		sudo apt-get upgrade
+	  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+		sudo apt-get install g++-4.7 -y c++-4.7 -y
+		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+		sudo apt-get install screen -y
+		sudo apt-get install tmux -y
+		sudo apt-get install libstdc++6 -y
+		sudo apt-get install lua-lgi -y
+		sudo apt-get install libnotify-dev -y
+  sudo service redis-server restart
+	wget http://valtman.name/files/telegram-cli-1222
+  mv telegram-cli-1222 telegram-cli
+   chmod +x telegram-cli
+   chmod +x anticrash.sh
+  install_luarocks
+  install_rocks
+  exit 1
 }
 
-ins3() {
+sudos() {
+  read -p "Do you want to update the project? [y/n] = "
+	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
+        sudo apt-get update
+	    wait
+	    sudo apt-get upgrade
+	    wait
+	    sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev -y
+	    wait
+		install2
+    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
+        install2
+	else
+	    sudos
+    fi
+}
+
+install() {
+  read -p "Do you want to update the project? [y/n] = "
+	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
+        sudo apt-get update
+	    wait
+	    sudo apt-get upgrade
+	    wait
+	    sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev -y
+	    wait
+		install2
+    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
+        install2
+	else
+	    sudos
+    fi
+}
+
+que() {
+read -p "Do you want to install files? [y/n] = "
+	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
+        install
+    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
+        exit 1
+	else
+	    que
+fi
+}
+
+error() {
 sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev tmux subversion
-
-  sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean && sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev tmux subversion -y
-
-   sudo apt-get update
-     sudo apt-get install -y python-software-properties software-properties-common
-        sudo add-apt-repository -y ppa:pi-rho/dev
-           sudo apt-get update
-              sudo apt-get install -y tmux=2.0-1~ppa1~t
-              
-                  sudo apt-get -y install libreadline6 libreadline-dev libreadline-dev libreadline6-dev libconfig-dev libssl-dev tmux lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip redis-server autoconf git g++ libjansson-dev libpython-dev expat libexpat1-dev ppa-purge python3-pip python3-dev software-properties-common python-software-properties gcc-6
-
-                      sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-                          sudo apt-get update
-                              sudo apt-get upgrade
-                                 sudo apt-get dist-upgrade
-                                     sudo ppa-purge
-                                        sudo apt-get install python3-pip
-                                           pip3 install redis
-                                               sudo apt-get update; sudo apt-get upgrade
-                                                   sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-
-sudo apt-get install lua-lgi
- sudo apt-get install software-properties-common
-   sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
-    sudo apt-get install libstdc++6
-     sudo apt-get update
-      sudo apt-get upgrade
-        sudo apt-get dist-upgrade
-
-wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
- tar zxpf luarocks-2.2.2.tar.gz
- cd luarocks-2.2.2
- ./configure; sudo make bootstrap
- sudo luarocks install luasocket
- sudo luarocks install luasec
- sudo luarocks install redis-lua
- sudo luarocks install lua-term
- sudo luarocks install serpent
- sudo luarocks install oauth
- sudo luarocks install fakeredis
- sudo luarocks install dkjson
- sudo luarocks install xml
- sudo luarocks install feedparser
- sudo luarocks install lanes
- sudo luarocks install Lua-cURL
- sudo service redis-server start
- cd ..
- rm -rf luarocks-2.2.2.tar.gz
- sudo apt-get install python-setuptools python-dev build-essential
- sudo easy_install pip
- sudo pip install redis
- }
-  
- ins4() {
-sudo apt-get install -y tor
-sudo apt-get install -y openvpn
-sudo apt-get install network-manager-openvpn
-
-sudo service tor start
-sudo service openvpn start
-
-export http_proxy="socks5://127.0.0.1:9150"
-export https_proxy="socks5://127.0.0.1:9150"
-export ftp_proxy="socks5://127.0.0.1:9150"
-
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install -f
-sudo dpkg -a --configure
-sudo apt-get dist-upgrade
-sudo dpkg --configure -a
-sudo sudo apt-get dist-upgrade
-            
-            		sudo apt-get install screen -y
-               		sudo apt-get install tmux -y
-                 		sudo apt-get install libstdc++6 -y
-                   		sudo apt-get install lua-lgi -y
-	                     	sudo apt-get install libnotify-dev -y
-
-sudo luarocks install redis
-sudo service redis-server restart -y
 }
 
-up() {
-cd $HOME && rm -rf .telegram-cli && rm -rf TabChi && rm -rf Tabchi && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean && git clone https://github.com/tgMember/TabChi.git && cd TabChi && chmod 777 install.sh && ./install.sh conf
-}
+#By: @sajjad_021
+if [ "$1" = "install" ]; then
+  install
+elif [ "$1" = "update" ]; then
+  update
+elif [ "$1" = "error" ]; then
+  error
+else
+  if [ ! -f telegram-cli ]; then
+    echo -e "\033[38;5;208mError! Tg not found, Please reply to this message:\033[0;00m"
+    read -p "Do you want to install starter files? [y/n] = "
+	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
+        install
+    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
+        exit 1
+	else
+	    que
+    fi
+  fi
 
-if [ "$1" = "up" ]; then
-  up
+  if [ ! -f telegram-cli ]; then
+    echo -e "\033[38;5;208mError! Tg not found, Please reply to this message:\033[0;00m"
+    read -p "Do you want to install starter files? [y/n] = "
+	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
+        install
+    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
+        exit 1
+	else
+	    que
+    fi
 fi
 
-if [ "$1" = "conf" ]; then
-onf
-ins1
-ins2
-ins3
-ins4
+  lua creator.lua
 fi
-
-   echo -e "\033[38;5;208m"
-   echo -e "install compelet"
-   echo -e "creat you robot whith  >>>  ./install.sh"
-   echo -e "\033[0;00m"
-   echo -e "\e[36m"
-
-lua creator.lua
